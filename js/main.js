8bfff75fe5b7788de092f46ba07c9945b303217b
anPeach <nastyapisarenko2001@gmail.com>
2021-06-04 01:07:27 +0300

    Исправила синтаксические ошибки

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
 const maxStringLength = 140;
 
-let getRandomValue = (minValue, maxValue) => {
+const getRandomValue = (minValue, maxValue) => {
   if (minValue < 0) {
-    throw new RangeError("The minimum value must be greater than 0");
+    throw new RangeError('The minimum value must be greater than 0');
   }
 
   if (maxValue <= minValue) {
-    throw new RangeError("The minimum value must be less than the maximum");
+    throw new RangeError('The minimum value must be less than the maximum');
   }
 
   return Math.round(Math.random() * (maxValue - minValue) + minValue);
 };
 
 try {
-  const result = getRandomValue(0, 1);
-  console.log(result);
+  getRandomValue(0, 1);
 } catch (err) {
-  console.log(err);
+  //console.log(err);
 }
 
 
-let checkStringLength = (string, maxLength) => {
-  return string.length <= maxLength ? true : false;
-}
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-const lengthController = checkStringLength('Hello, my name is Nastya! But some people on the internet call me AnPeach.', maxStringLength);
-console.log(lengthController);
+checkStringLength('Hello, my name is Nastya! But some people on the internet call me AnPeach.', maxStringLength);
