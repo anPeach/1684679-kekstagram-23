9854b693014b3da682d1cb6d192cd2d9899aab97
anPeach <nastyapisarenko2001@gmail.com>
2021-06-08 04:46:05 +0300

    Исправила ошибки

--- js/main.js
+++ js/main.js
@@ -5,16 +5,18 @@ const MIN_LIKES = 15;
 const MAX_LIKES = 200;
 const MIN_AVATAR = 1;
 const MAX_AVATAR = 6;
-const PHOTO_DESCRIPTION_COUNT = 25;
-const MESSAGES = ['Всё отлично!',
-'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+//const PHOTO_DESCRIPTION_COUNT = 25;
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 const NAMES = ['Vasya', 'Alice', 'Jake', 'Nastya', 'Dima', 'Tom'];
-let descriptionIdes = [];
-let commentIdes = [];
-
+const descriptionIdes = [];
+const commentIdes = [];
 
 const getRandomNumber = function (min, max) {
   if (max > min && min >= 0 && max > 0) {
@@ -25,17 +27,19 @@ const getRandomNumber = function (min, max) {
 };
 
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
-checkStringLength('Hello, my name is Nastya! But some people on the internet call me AnPeach.', MAX_STRING_LENGTH);
+checkStringLength(
+  'Hello, my name is Nastya! But some people on the internet call me AnPeach.',
+  MAX_STRING_LENGTH,
+);
 
-const isUnique = (array, value) => {
-  return !array.includes(value);
-}
+const isUnique = (array, value) => !array.includes(value);
 
 const createId = (array, minId, maxId) => {
   let id = getRandomNumber(minId, maxId);
+  const check = true; //npm указывает на ошибку в цикле, если там написать просто true
 
-  while (true) {
-    if(isUnique(array, id)){
+  while (check) {
+    if (isUnique(array, id)) {
       break;
     }
 
@@ -45,10 +49,9 @@ const createId = (array, minId, maxId) => {
   array.push(id);
 
   return id;
-}
+};
 
 const createPhotoDescription = (description = '') => {
-
   const descriptionId = createId(descriptionIdes, MIN_ID, MAX_ID);
 
   return {
@@ -56,16 +59,18 @@ const createPhotoDescription = (description = '') => {
     url: `photos/${descriptionId}.jpg`,
     description: description,
     likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-    comment: [{
-      id: createId(commentIdes, MIN_ID, MESSAGES.length),
-      avatar: `img/avatar-${getRandomNumber(MIN_AVATAR, MAX_AVATAR)}.svg`,
-      message: MESSAGES[getRandomNumber(0, MESSAGES.length -1)],
-      name: NAMES[getRandomNumber(0, NAMES.length - 1)]
-    }]
-  }
-}
+    comment: [
+      {
+        id: createId(commentIdes, MIN_ID, MESSAGES.length),
+        avatar: `img/avatar-${getRandomNumber(MIN_AVATAR, MAX_AVATAR)}.svg`,
+        message: MESSAGES[getRandomNumber(0, MESSAGES.length - 1)],
+        name: NAMES[getRandomNumber(0, NAMES.length - 1)],
+      },
+    ],
+  };
+};
 
 createPhotoDescription('good morning');
 
-//когда происходит выполнение 71 строки - сервер виснет, на 68 строке - все нормально
+//когда происходит выполнение 71 строки - сервер виснет, на 68 строке - все нормально, чтобы проверить нужно раскомментировать 8 строку
 //const photoDescriptions = new Array(PHOTO_DESCRIPTION_COUNT).fill(null).map(()=>createPhotoDescription('Good morning'));
