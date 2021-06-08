184713d592c9907b7e661dafb90687b4ef35d4cd
anPeach <nastyapisarenko2001@gmail.com>
2021-06-08 04:32:06 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-const maxStringLength = 140;
+const MAX_STRING_LENGTH = 140;
+const MIN_ID = 1;
+const MAX_ID = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_AVATAR = 1;
+const MAX_AVATAR = 6;
+const PHOTO_DESCRIPTION_COUNT = 25;
+const MESSAGES = ['Всё отлично!',
+'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const NAMES = ['Vasya', 'Alice', 'Jake', 'Nastya', 'Dima', 'Tom'];
+let descriptionIdes = [];
+let commentIdes = [];
+
 
 const getRandomNumber = function (min, max) {
   if (max > min && min >= 0 && max > 0) {
@@ -8,8 +24,48 @@ const getRandomNumber = function (min, max) {
   throw new RangeError('error here');
 };
 
-getRandomNumber(2, 5);
-
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
+checkStringLength('Hello, my name is Nastya! But some people on the internet call me AnPeach.', MAX_STRING_LENGTH);
 
-checkStringLength('Hello, my name is Nastya! But some people on the internet call me AnPeach.', maxStringLength);
+const isUnique = (array, value) => {
+  return !array.includes(value);
+}
+
+const createId = (array, minId, maxId) => {
+  let id = getRandomNumber(minId, maxId);
+
+  while (true) {
+    if(isUnique(array, id)){
+      break;
+    }
+
+    id = getRandomNumber(minId, maxId);
+  }
+
+  array.push(id);
+
+  return id;
+}
+
+const createPhotoDescription = (description = '') => {
+
+  const descriptionId = createId(descriptionIdes, MIN_ID, MAX_ID);
+
+  return {
+    id: descriptionId,
+    url: `photos/${descriptionId}.jpg`,
+    description: description,
+    likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+    comment: [{
+      id: createId(commentIdes, MIN_ID, MESSAGES.length),
+      avatar: `img/avatar-${getRandomNumber(MIN_AVATAR, MAX_AVATAR)}.svg`,
+      message: MESSAGES[getRandomNumber(0, MESSAGES.length -1)],
+      name: NAMES[getRandomNumber(0, NAMES.length - 1)]
+    }]
+  }
+}
+
+createPhotoDescription('good morning');
+
+//когда происходит выполнение 71 строки - сервер виснет, на 68 строке - все нормально
+//const photoDescriptions = new Array(PHOTO_DESCRIPTION_COUNT).fill(null).map(()=>createPhotoDescription('Good morning'));
